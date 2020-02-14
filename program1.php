b2662ea3309779be18dba2326a36f2660f4129d1
8506950 <61038391+8506950@users.noreply.github.com>
2020-02-14 12:48:26 +0530

    Update program1.php

--- program1.php
+++ program1.php
@@ -2,9 +2,5 @@
 $food = array('fruits' => array('orange', 'banana', 'apple'),
               'veggie' => array('carrot', 'collard', 'pea'));
 
-// recursive count
-echo count($food, COUNT_RECURSIVE); // output 8
-//var_dump(count($food, COUNT_RECURSIVE));
-
 
 ?>
