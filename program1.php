4b9d0678071072aff8f39d6f1139acd0bc244405
8506950 <61038391+8506950@users.noreply.github.com>
2020-02-14 12:45:57 +0530

    Update program1.php

--- program1.php
+++ program1.php
@@ -6,7 +6,5 @@ $food = array('fruits' => array('orange', 'banana', 'apple'),
 echo count($food, COUNT_RECURSIVE); // output 8
 //var_dump(count($food, COUNT_RECURSIVE));
 
-// normal count
-echo count($food); // output 2
-//var_dump($food);
+
 ?>
